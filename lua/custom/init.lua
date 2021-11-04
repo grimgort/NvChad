5014f58b573b5e17946ba52ca2b6c376ea3dba3d
frederic <frederic.tarroux@onera.fr>
2021-11-04 13:07:17 +0100

    chich key mapping

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -24,6 +24,8 @@ hooks.add("setup_mappings", function(map)
    -- vim.api.nvim_set_keymap("n", "<leader>cc", "gg0vG$d", opt) -- example to delete the buffer
    local opt = {}
    local opts = {}
+   vim.api.nvim_set_keymap("i", "é", "é", opt)
+   vim.api.nvim_set_keymap("i", "^", "^", opt)
    vim.api.nvim_set_keymap("n", " gy", ":Gina tag<cr>", opt)
    vim.api.nvim_set_keymap("n", " wx", ":only<cr>", opt)
    vim.api.nvim_set_keymap("n", " gb", ":Gina branch<cr>", opt)
@@ -137,6 +139,35 @@ hooks.add("setup_mappings", function(map)
    vim.api.nvim_set_keymap("n", "<leader>wh", [[:%s/<c-r><c-w>/<c-r><c-w>/g]], {})
    vim.api.nvim_set_keymap("n", "<leader>gc", ":Telescope grep_string<cr>", {})
    -- vim.api.nvim_set_keymap("c","<S-k>","<Up>",opts)
+   local wk = require "which-key"
+   wk.register {
+      ["<leader>"] = {
+         f = {
+            name = "+file",
+         },
+         g = {
+            name = "+git",
+            t = {
+               name = "+telescope",
+            },
+         },
+         h = {
+            name = "+hop",
+         },
+         n = {
+            name = "+LSP",
+         },
+         t = {
+            name = "+telescope",
+         },
+         u = {
+            name = "+nvchad",
+         },
+         w = {
+            name = "+basicVim",
+         },
+      },
+   }
 end)
 
 -- To add new plugins, use the "install_plugin" hook,
@@ -160,29 +191,75 @@ hooks.add("install_plugins", function(use)
       "folke/which-key.nvim",
       config = function()
          require("which-key").setup {
-
-            --[[ key_labels = {
-    ["<space>"] = "SPC",
-    ["<CR>"] = "RET",
-    ["<tab>"] = "TAB",
-    ["g"] = "g_git",
-    ["h"] = "h_hop",
-    ["!"] = "!_poubelle",
-    ["b"] = "b_dashboard",
-    ["c"] = "c_directory",
-    ["f"] = "f_find_files",
-    ["n"] = "n_LSP",
-    ["t"] = "t_telescope",
-    ["u"] = "u_NVCHAD",
-    ["w"] = "w_other",
-} ]]
-            -- require("which-key").register({g={name="git"}})
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
+            {
+               plugins = {
+                  marks = true, -- shows a list of your marks on ' and `
+                  registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+                  spelling = {
+                     enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+                     suggestions = 20, -- how many suggestions should be shown in the list?
+                  },
+                  -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+                  -- No actual key bindings are created
+                  presets = {
+                     operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+                     motions = true, -- adds help for motions
+                     text_objects = true, -- help for text objects triggered after entering an operator
+                     windows = true, -- default bindings on <c-w>
+                     nav = true, -- misc bindings to work with windows
+                     z = true, -- bindings for folds, spelling and others prefixed with z
+                     g = true, -- bindings for prefixed with g
+                  },
+               },
+               -- add operators that will trigger motion and text object completion
+               -- to enable all native operators, set the preset / operators plugin above
+               operators = { gc = "Comments" },
+               key_labels = {
+                  -- override the label used to display some keys. It doesn't effect WK in any other way.
+                  -- For example:
+                  -- ["<space>"] = "SPC",
+                  -- ["<cr>"] = "RET",
+                  -- ["<tab>"] = "TAB",
+               },
+               icons = {
+                  breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+                  separator = "➜", -- symbol used between a key and it's label
+                  group = "+", -- symbol prepended to a group
+               },
+               popup_mappings = {
+                  scroll_down = "<c-d>", -- binding to scroll down inside the popup
+                  scroll_up = "<c-u>", -- binding to scroll up inside the popup
+               },
+               window = {
+                  border = "none", -- none, single, double, shadow
+                  position = "bottom", -- bottom, top
+                  margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+                  padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+                  winblend = 0,
+               },
+               layout = {
+                  height = { min = 4, max = 25 }, -- min and max height of the columns
+                  width = { min = 20, max = 50 }, -- min and max width of the columns
+                  spacing = 3, -- spacing between columns
+                  align = "left", -- align columns left, center or right
+               },
+               ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+               hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+               show_help = true, -- show help message on the command line when the popup is visible
+               triggers = "auto", -- automatically setup triggers
+               -- triggers = {"<leader>"} -- or specify a list manually
+               triggers_blacklist = {
+                  -- list of mode / prefixes that should never be hooked by WhichKey
+                  -- this is mostly relevant for key maps that start with a native binding
+                  -- most people should not need to change this
+                  i = { "j", "k", "é", "^" },
+                  v = { "j", "k", "é", "^" },
+               },
+            },
          }
       end,
    }
+
    use {
       "akinsho/toggleterm.nvim",
       config = function()
