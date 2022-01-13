d10caea30735937447c7ad65d13f5e0224a5d59f
frederic <frederic.tarroux@onera.fr>
2022-01-13 13:57:59 +0100

    adding totcometn

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -913,6 +913,18 @@ require("telescope").load_extension "neovim-session-manager"
       end,
       requires = "nvim-lua/plenary.nvim",
    }
+
+use {
+  "folke/todo-comments.nvim",
+  requires = "nvim-lua/plenary.nvim",
+  config = function()
+    require("todo-comments").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
    -- use{"oberblastmeister/neuron.nvim"}
    -- use{"funorpain/vim-cpplint"}
    -- use {
